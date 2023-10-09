151297a89e2ef5171a4dad8b4e1faa0210902325
Brigitte91 <71765969+Brigitte91@users.noreply.github.com>
2023-10-09 15:28:59 +0200

    Update PluginLogin.php

--- PluginLogin.php
+++ PluginLogin.php
@@ -2,102 +2,94 @@
 // Plugin Name: GSS API login call
 // Plugin URI: https://www.gsslogistics.nl/
 // Description: API caller login TMS-ForYou
-// Author: .
+// Author: Brigitte
 
-//
-   function enqueue_my_scripts() {
+function enqueue_my_scripts() {
     wp_enqueue_script('jquery');
-    // Here enqueue your script
-    }
+}
+
 function loginForm( $atts ) {
-	$error = ( get_query_var( 'error' ) ) ? get_query_var( 'error' ) : false;
+    $error = ( get_query_var( 'error' ) ) ? get_query_var( 'error' ) : false;
 
-	$dashboard    = 'gss'; // Default dashboard value
-	$errorMessage = '';
+    $dashboard    = 'gss'; // Default dashboard value
+    $errorMessage = '';
 
-	if ( $error == 'ok' ) {
-		$errorMessage = '<div class="error">Gegevens onjuist, probeer het opnieuw.</div>';
-	} elseif ( $error == 'gebruikersnaam' || $error == 'incorrect' ) {
-		$errorMessage = '<div class="error">Gebruikersnaam of wachtwoord niet volledig of incorrect.</div>';
-	}
+    if ( $error == 'ok' ) {
+        $errorMessage = '<div class="error">Gegevens onjuist, probeer het opnieuw.</div>';
+    } elseif ( $error == 'gebruikersnaam' || $error == 'incorrect' ) {
+        $errorMessage = '<div class="error">Gebruikersnaam of wachtwoord niet volledig of incorrect.</div>';
+    }
 
-	$content = <<<HTML
-        <div class="loginform">
-            
-            $errorMessage
-            
-            <div class="error voorwaarden">
-                Vergeet niet akkoord te gaan met de voorwaarden.
-            </div>
-            
-            <form 
-                id="loginformulier" 
-                method="post" 
-                action="https://webservices.tms-foryou.nl/webservices/loginkey?dashboard={$dashboard}">
-                
-                <input 
-                    type="text" 
-                    placeholder="Gebruikersnaam" 
-                    name="gebruikersnaam" />
-
-                <input 
-                    type="password" 
-                    placeholder="Wachtwoord" 
-                    name="wachtwoord" />
-
-                <input 
-                    type="checkbox" 
-                    id="voorwaarden"> 
-                
-                <label for="voorwaarden">
-                    Ik ga akkoord met de 
-                    <a 
-                        target="_blank" 
-                        href="https://globalsmartshipping.nl/algemene-voorwaarden/">
-                        voorwaarden
-                    </a> 
-                    en 
-                    <a 
-                        target="_blank" 
-                        href="https://globalsmartshipping.nl/privacy-statement/">
-                            privacyverklaring
-                    </a>
-                </label>
-                <input 
-                    onclick="inloggenStart(event)" 
-                    type="submit" 
-                    value="Inloggen" />
-
-                <a 
-                    href="https://www.rijologistics.nl/wachtwoord-vergeten/" 
-                    class="wwlinkje">
-                    Wachtwoord vergeten? Klik hier
-                </a>
-
-            </form>
+    $content = <<<HTML
+    <div class="loginform">
+        $errorMessage
+        
+        <div class="error voorwaarden">
+            Vergeet niet akkoord te gaan met de voorwaarden.
         </div>
+        
+        <form 
+            id="loginformulier" 
+            method="post" 
+            action="https://webservices.tms-foryou.nl/webservices/loginkey?dashboard={$dashboard}">
+            
+            <input 
+                type="text" 
+                placeholder="Gebruikersnaam" 
+                name="gebruikersnaam" />
 
-        <script>
-        jQuery(document).ready(function($) {
-            function inloggenStart(e) {
-                e.preventDefault();
-                if (jQuery("#voorwaarden").is(":checked")) {
-                    jQuery("#loginformulier").submit();
-                } else {
-                    jQuery(".foutmelding.voorwaarden").fadeIn();
-                }
-            }
-            });
-        </script>
+            <input 
+                type="password" 
+                placeholder="Wachtwoord" 
+                name="wachtwoord" />
 
+            <input 
+                type="checkbox" 
+                id="voorwaarden"> 
+            
+            <label for="voorwaarden">
+                Ik ga akkoord met de 
+                <a 
+                    target="_blank" 
+                    href="https://globalsmartshipping.nl/algemene-voorwaarden/">
+                    voorwaarden
+                </a> 
+                en 
+                <a 
+                    target="_blank" 
+                    href="https://globalsmartshipping.nl/privacy-statement/">
+                        privacyverklaring
+                </a>
+            </label>
+            <input 
+                onclick="inloggenStart(event)" 
+                type="submit" 
+                value="Inloggen" />
 
+            <a 
+                href="https://www.rijologistics.nl/wachtwoord-vergeten/" 
+                class="wwlinkje">
+                Wachtwoord vergeten? Klik hier
+            </a>
 
+        </form>
+    </div>
+
+    <script>
+    function inloggenStart(e) {
+        e.preventDefault();
+        if (jQuery("#voorwaarden").is(":checked")) {
+            jQuery("#loginformulier").submit();
+        } else {
+            jQuery(".foutmelding.voorwaarden").fadeIn();
+        }
+    }
+    </script>
     HTML;
 
+    add_action('wp_enqueue_scripts', 'enqueue_my_scripts');
 
-    add_action('wp_enqueue_script', 'enqueue_my_scripts');
-
-	return $content;
+    return $content;
 }
 
 add_shortcode( 'loginformulier', 'loginForm' );
